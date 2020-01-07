789311b29d69f5e386440e0f98cc48b0343eedfd
edward <edward@utexas.edu>
2020-01-07 11:17:18 -0800

    Initial Commit

---
+++ db/migrate/20200107171248_create_friendships.rb
@@ -0,0 +1,11 @@
+# class CreateFriendships < ActiveRecord::Migration[5.2]
+#   def change
+#     create_table :friendships do |t|
+#       t.string :user_one_id, null: false
+#       t.string :user_two_id, null: false
+
+#       t.timestamps
+#     end
+#     add_index :friendships, [:user_one_id, :user_two_id], unique: true
+#   end
+# end
